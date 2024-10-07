8b7cfd5ee79cf548fa062718e338e230a161940b
mandeukJeong <wjdalstj137@gmail.com>
2024-10-07 12:17:44 +0900

    5. 배열: 모의고사(refactoring)

--- array/mock_exam.js
+++ array/mock_exam.js
@@ -1,60 +1,31 @@
 function solution(answers) {
-  let maxNum = 0;
-  const student1 = [1, 2, 3, 4, 5];
-  const student2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
 
-  const count = [0, 0, 0];
+  const scores = [0, 0, 0];
 
-  const result = [];
-
-  for (let i = 0; i < answers.length; i++) {
-    let tmp = i;
-
-    if (student1.length < answers.length) {
-      tmp = i - student1.length;
-    } else {
-      tmp = i;
-    }
-
-    if (answers[i] === student1[tmp]) {
-      count[0]++;
-    }
-
-    if (student2.length < answers.length) {
-      tmp = i - student2.length;
-    } else {
-      tmp = i;
-    }
-
-    if (answers[i] === student2[tmp]) {
-      count[1]++;
-    }
-
-    if (student3.length < answers.length) {
-      tmp = i - student3.length;
-    } else {
-      tmp = i;
-    }
-
-    if (answers[i] === student3[tmp]) {
-      count[2]++;
+  for (const [i, answer] of answers.entries()) {
+    for (const [j, pattern] of patterns.entries()) {
+      if (answer === pattern[i % pattern.length]) {
+        scores[j] += 1;
+      }
     }
   }
 
-  for (let i = 0; i < 3; i++) {
-    if (maxNum <= count[i]) {
-      maxNum = count[i];
+  const maxScore = Math.max(...scores);
+
+  const highestScores = [];
+
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] === maxScore) {
+      highestScores.push(i + 1);
     }
   }
 
-  for (let i = 0; i < 3; i++) {
-    if (maxNum === count[i]) {
-      result.push(i + 1);
-    }
-  }
-
-  return result;
+  return highestScores;
 }
 
 console.log(solution([1, 2, 3, 4, 5]));

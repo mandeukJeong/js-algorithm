b117757ef31184fae04a40da96219658500b201d
mandeukJeong <wjdalstj137@gmail.com>
2024-10-08 22:17:07 +0900

    6. 스택: 짝지어 제거하기

---
+++ stack/remove_pairs.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length === 0) {
+      stack.push(s[i]);
+    } else {
+      if (stack[stack.length - 1] === s[i]) {
+        stack.pop();
+      } else {
+        stack.push(s[i]);
+      }
+    }
+  }
+
+  return stack.length === 0 ? 1 : 0;
+}
+
+console.log(solution('baabaa'));
+console.log(solution('cdcd'));

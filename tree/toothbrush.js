9415dd2199d37adf32002cba88b7a4fed8ea51a0
mandeukJeong <wjdalstj137@gmail.com>
2024-10-18 23:22:14 +0900

    8. 트리: 다단계 칫솔 판매

---
+++ tree/toothbrush.js
@@ -0,0 +1,35 @@
+function solution(enroll, referral, seller, amount) {
+  let parent = {};
+
+  for (let i = 0; i < enroll.length; i++) {
+    parent[enroll[i]] = referral[i];
+  }
+
+  let total = {};
+  for (let name of enroll) {
+    total[name] = 0;
+  }
+
+  for (let i = 0; i < seller.length; i++) {
+    let money = amount[i] * 100;
+    let curName = seller[i];
+
+    while (money > 0 && curName != '-') {
+      total[curName] += money - Math.floor(money / 10);
+      curName = parent[curName];
+
+      money = Math.floor(money / 10);
+    }
+  }
+
+  return enroll.map((name) => total[name]);
+}
+
+console.log(
+  solution(
+    ['john', 'mary', 'edward', 'sam', 'emily', 'jaimie', 'tod', 'young'],
+    ['-', '-', 'mary', 'edward', 'mary', 'mary', 'jaimie', 'edward'],
+    ['young', 'john', 'tod', 'emily', 'mary'],
+    [12, 4, 2, 5, 10]
+  )
+);

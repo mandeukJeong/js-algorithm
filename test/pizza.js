9717b9b3491dbccf4c8bc41cbcfa988b27b33b2e
mandeukJeong <wjdalstj137@gmail.com>
2025-01-17 21:36:00 +0900

    코딩 테스트: 피자 나눠 먹기

---
+++ test/pizza.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  var answer = 0;
+
+  if (n <= 7) {
+    answer = 1;
+  } else {
+    if (n % 7 === 0) {
+      answer = Math.trunc(n / 7);
+    } else {
+      answer = Math.trunc(n / 7) + 1;
+    }
+  }
+  return answer;
+}

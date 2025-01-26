64910c4e98a408e0bd9436ef3321a1be79c51966
mandeukJeong <wjdalstj137@gmail.com>
2025-01-26 23:41:13 +0900

    코딩 테스트: 베스트 앨범

---
+++ test/albem.js
@@ -0,0 +1,32 @@
+function solution(genres, plays) {
+  let answer = [];
+  const genresObj = {};
+  const playObj = {};
+
+  for (let i = 0; i < genres.length; i++) {
+    genre = genres[i];
+    play = plays[i];
+
+    if (!(genre in genresObj)) {
+      genresObj[genre] = [];
+      playObj[genre] = 0;
+    }
+
+    genresObj[genre].push([i, play]);
+    playObj[genre] += play;
+  }
+
+  sortedGenres = Object.keys(playObj).sort((a, b) => {
+    return playObj[b] - playObj[a];
+  });
+
+  for (const genre of sortedGenres) {
+    sortedSongs = genresObj[genre].sort((a, b) => {
+      return a[1] === b[1] ? a[0] - b[0] : b[1] - a[1];
+    });
+
+    answer.push(...sortedSongs.slice(0, 2).map((song) => song[0]));
+  }
+
+  return answer;
+}
